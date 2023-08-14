37ab7220beb33432f2cf9ae3022c0c124e9f2bcc
vitor hugo <vitor.oliveira@altbank.co>
2023-08-14 09:56:53 -0300

    feat: fixing the splash

--- lib/src/splash/splash.dart
+++ lib/src/splash/splash.dart
@@ -1,17 +1,20 @@
-import 'package:cloudwalknasa/main.dart';
-import 'package:cloudwalknasa/services/audio.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<StatefulWidget> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen>
     with TickerProviderStateMixin {
   late AnimationController _controller;
-  late List<Animation<double>> _animations;
-  final String textToDisplay = 'NASA Gallery'.toUpperCase();
+  late Animation<double> _logoAnimation;
+  late Animation<double> _firstWordAnimation;
+  late Animation<double> _secondWordAnimation;
+  bool isAnimated = true;
+  List<String> words = ["NASA", "Gallery".toUpperCase()];
 
   @override
   void initState() {
@@ -19,84 +22,85 @@ class _SplashScreenState extends State<SplashScreen>
 
     _controller = AnimationController(
       vsync: this,
-      duration: Duration(seconds: textToDisplay.length~/2),
+      duration: const Duration(seconds: 6), // 2 seconds for each part
     );
 
-    _animations = List.generate(
-      textToDisplay.length,
-          (index) => Tween<double>(begin: 0.0, end: 1.0).animate(
-        CurvedAnimation(
-          parent: _controller,
-          curve: Interval(
-            index / textToDisplay.length,
-            (index + 1) / textToDisplay.length,
-            curve: Curves.easeIn,
-          ),
+    _logoAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(
+          0.0,
+          1.0 / 3, // Appear in the first 2 seconds
+          curve: Curves.easeIn,
         ),
       ),
     );
 
-    AudioManager().play(); // Start playing music
+    _firstWordAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(
+          1.0 / 3,
+          2.0 / 3, // Appear in the second 2 seconds
+          curve: Curves.easeIn,
+        ),
+      ),
+    );
+
+    _secondWordAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(
+          2.0 / 3,
+          1.0, // Appear in the last 2 seconds
+          curve: Curves.easeIn,
+        ),
+      ),
+    );
 
     _controller.forward();
-
-    // Navigate to the next page after 6 seconds + the duration of the animation
-    Future.delayed(Duration(seconds: 6 + textToDisplay.length), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomePage()),
-      );
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.transparent,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             FadeTransition(
-              opacity: _controller,
-              child: Image.asset('assets/nasa-logo.png'),
+              opacity: _logoAnimation,
+              child: Image.asset('assets/nasa-logo.png', width: 300),
             ),
             const SizedBox(height: 20),
-            _animatedText(0, 4), // NASA
-            _animatedText(5, textToDisplay.length), // Gallery
+            FadeTransition(
+              opacity: _firstWordAnimation,
+              child: Text(
+                words[0],
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Nasa",
+                ),
+              ),
+            ),
+            FadeTransition(
+              opacity: _secondWordAnimation,
+              child: Text(
+                words[1],
+                style: const TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: "Nasa",
+                ),
+              ),
+            ),
           ],
         ),
       ),
-    );
-  }
-
-  Widget _animatedText(int start, int end) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: _animations
-          .sublist(start, end)
-          .asMap()
-          .entries
-          .map((entry) => _animatedLetter(entry.key + start))
-          .toList(),
-    );
-  }
-
-  Widget _animatedLetter(int index) {
-    return AnimatedBuilder(
-      animation: _animations[index],
-      builder: (context, snapshot) {
-        return Opacity(
-          opacity: _animations[index].value,
-          child: Text(
-            textToDisplay[index],
-            style: const TextStyle(
-              fontSize: 30,
-              color: Colors.white,
-            ),
-          ),
-        );
-      },
     );
   }
 
@@ -106,5 +110,3 @@ class _SplashScreenState extends State<SplashScreen>
     super.dispose();
   }
 }
-
-
